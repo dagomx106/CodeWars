d7f862465ea53cf246227aab2d711eb166fc81eb
dagomx106 <dagovf@outlook.com>
2024-05-04 22:40:16 -0600

    Adding more

--- FactDecomp.java
+++ FactDecomp.java
@@ -1,5 +1,7 @@
 package codewars;
 
+import java.util.Date;
+
 public class FactDecomp {
 
         public static String decomp(int n) {
@@ -28,6 +30,7 @@ public class FactDecomp {
 
     public static void main(String[] args) {
         System.out.println(decomp(22));
+
     }
 
     }
